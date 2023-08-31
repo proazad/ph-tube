3b7393997cd60dcd59511132f05a3379ae3eefac
proazad <proazadhossain7595@gmail.com>
2023-08-31 15:55:54 +0300

    Header Complete and Full Project Setup Complete

---
+++ assets/scripts/app.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Load All Data 
+ */
+const loadData = async () => {
+    const url = `https://openapi.programming-hero.com/api/videos/categories`;
+    const response = await fetch(url);
+    const data = await response.json();
+    // const da = data;
+    console.log(data)
+
+}
+loadData();
