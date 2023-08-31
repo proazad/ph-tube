e8748cefbd211104e06280e38b4621f9d55655f9
proazad <proazadhossain7595@gmail.com>
2023-08-31 17:30:53 +0300

    Dynamicly All Category Loaded

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,31 @@
 /**
  * 
- * Load All Data 
+ * Load All Category 
  */
-const loadData = async () => {
+const loadCategory = async () => {
     const url = `https://openapi.programming-hero.com/api/videos/categories`;
     const response = await fetch(url);
     const data = await response.json();
-    // const da = data;
-    console.log(data)
-
+    const categories = data.data;
+    const categotyContainer = document.getElementById('categroyContainer');
+    categories.forEach(category => {
+      const btn = document.createElement("button");
+      btn.classList = 'btn btn-md hover:text-black';
+      btn.innerText = category.category;
+      categotyContainer.appendChild(btn);
+    });
 }
-loadData();
 
+/**
+ * Display or Load All Data 
+ */
+const displayData = async (id='1000') =>{
+  const url = `https://openapi.programming-hero.com/api/videos/category/${id}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const videos = data.data;
+  console.log(videos);
+}
+
+loadCategory();
+displayData();
